2abd95439f161c76354f511a52cf8fc2c940f002
Brady Neal <bradyneal11@gmail.com>
2017-07-14 11:28:36 -0400

    Add data whitening and fix associativity bug

--- preprocessing/transform.py
+++ preprocessing/transform.py
@@ -6,6 +6,7 @@ and standardization, or even whitening, of non-categorical variables.
 
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.decomposition import PCA
 
 from .separation import separate_categorical
 
@@ -48,7 +49,7 @@ def standardize(X, scaler=None, return_scaler=False):
         standardization information of X (training set)
     """
     if X.shape[1] == 0:
-        return X, None if return_scaler else X
+        return (X, scaler) if return_scaler else X
     if scaler is None:
         scaler = StandardScaler().fit(X)
     if return_scaler:
@@ -85,4 +86,58 @@ def standardize_and_one_hot(X, categorical, scaler=None, return_scaler=False):
     
     X_new = np.concatenate((X_scaled, X_one_hot), axis=1)
     return (X_new, scaler) if return_scaler else X_new
+
+
+def whiten(X, transform=None, return_transform=False):
+    """
+    Whiten data (transformation with identity covariance matrix). Assume it is
+    appropriate to whiten every feature in X (e.g. X has no categorical
+    variables).
     
+    Args:
+        X: data matrix
+        transform: if provided, use for whitening (e.g. for test set)
+        return_transform: if true, return transform (to later be used on test set)
+        
+    Returns:
+        whitened X and, optionally, the transform object that contains the
+        whitening information of X (training set)
+    """
+    if X.shape[1] == 0:
+        return (X, transform) if return_transform else X
+    if transform is None:
+        transform = PCA(whiten=True)
+        X_whitened = transform.fit_transform(X)
+    else:
+        X_whitened = transform.transform(X)
+    return (X_whitened, transform) if return_transform else X_whitened
+    
+
+def whiten_and_one_hot(X, categorical, transform=None, return_transform=False):
+    """
+    Whiten the non-categorical data and encode the categorical data in a
+    one-hot representation. Note: this functions changes the order of the
+    columns so that all of the non-categorical features come before the
+    newly one-hot encoded categorical features.
+    
+    Args:
+        X: data matrix
+        categorical: list of booleans indicating which features are categorical
+        transform: if provided, use for whitening (e.g. for test set)
+        return_transform: if true, return transform (to later be used on test set)
+        
+    Returns:
+        whitened and one-hot encoded X and, optionally, the transform object
+        that contains the whitening information of X (training set)
+    """
+    X_categ, X_non_categ = separate_categorical(X, categorical)
+    
+    if return_transform:
+        X_whitened, transform = whiten(X_non_categ, transform, return_transform)
+    else:
+        X_whitened = whiten(X_non_categ, transform, return_transform)
+        
+    X_one_hot = one_hot(X_categ)
+    
+    X_new = np.concatenate((X_whitened, X_one_hot), axis=1)
+    return (X_new, transform) if return_transform else X_new    
