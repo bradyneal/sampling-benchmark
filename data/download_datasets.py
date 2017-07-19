c577131da4c634ea102e4ac33dd4e51ad617554b
Brady Neal <bradyneal11@gmail.com>
2017-07-19 11:02:49 -0400

    Add code for loading from checkpoint and code for manually specifying index to start downloading at

--- data/download_datasets.py
+++ data/download_datasets.py
@@ -10,8 +10,9 @@ import openml
 from openml.exceptions import OpenMLServerError
 from requests.exceptions import ChunkedEncodingError
 
-OPENML_FOLDER = os.path.join(os.sep, *'/data/lisa/data/openml'.split('/'))
-DATASETS_FOLDER = os.path.join(OPENML_FOLDER, 'datasets')
+UNIX_OPENML_PATH = '/data/lisa/data/openml'
+OPENML_FOLDER = os.path.join(os.sep, *UNIX_OPENML_PATH.split('/'))
+DATASETS_FOLDER = os.path.join(OPENML_FOLDER, 'datasets', 'raw')
 CHECKPOINT_ITERS = 25
 
 
@@ -23,16 +24,31 @@ def get_dataset_ids():
     return filtered_df.did.values
     
     
-def download_datasets(dataset_ids, verbose=False):
+def download_datasets(dataset_ids, start_iteration=0, verbose=False):
     """Download the datasets that correspond to all of the give IDs"""
+    num_datasets = len(dataset_ids)
     good_dataset_ids = []
     bad_dataset_ids = []
-    num_datasets = len(dataset_ids)
     exceptions = []
-    for i, dataset_id in enumerate(dataset_ids):
+    
+    # load previous saved values of above variables
+    info_filename = get_info_filename()
+    if os.path.isfile(info_filename):
+        if verbose: print('Loading download info from file')
+        with open(info_filename, 'rb') as f:
+            info = pickle.load(f)
+        start_iteration = info['iteration']
+        good_dataset_ids = info['good_dataset_ids']
+        bad_dataset_ids = info['bad_dataset_ids']
+        exceptions = info['exceptions']
+
+    # loop through dataset_ids and download corresponding datasets
+    for i in range(start_iteration, num_datasets):
+        dataset_id = dataset_ids[i]
         if verbose:
             print('{} of {}\tdataset ID: {} ...' \
                   .format(i, num_datasets, dataset_id), end=' ')
+        # OpenML likes to throw all kinds of errors when getting datasets
         try:
             dataset = openml.datasets.get_dataset(dataset_id)
             good_dataset_ids.append(dataset_id)
@@ -43,6 +59,7 @@ def download_datasets(dataset_ids, verbose=False):
             bad_dataset_ids.append(dataset_id)
             exceptions.append(e)
             if verbose: print('Failure')
+        # checkpoint info
         if (i + 1) % CHECKPOINT_ITERS == 0:
             if verbose:
                 print('Reached iteration {}. Writing download info' \
@@ -95,5 +112,5 @@ def get_dataset_dict(dataset):
 
 
 if __name__ == '__main__':
-    download_datasets(get_dataset_ids(), verbose=True)
+    download_datasets(get_dataset_ids(), start_iteration=17676, verbose=True)
     
