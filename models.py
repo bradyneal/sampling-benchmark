8b2b215e5a010328a8e4193af9fe312abd9fc38a
Brady Neal <bradyneal11@gmail.com>
2017-07-24 13:39:14 -0400

    Add linear regression with second order terms

--- models.py
+++ models.py
@@ -6,12 +6,17 @@ constant.
 
 import pymc3 as pm
 from pymc3.backends.tracetab import trace_to_dataframe
+from itertools import combinations
 
-MODEL_NAMES = ['ls_linear', 'robust_linear']
+from data import numpy_to_dataframe, get_var_names
+
+MODEL_NAMES = ['ls_linear', 'ls_pairwise_linear', 'ls_quadratic_linear',
+               'robust_linear']
 NUM_SAMPLES = 500
 
 
-def sample_model(model_name, X, y, num_samples=NUM_SAMPLES):
+def sample_model(model_name, X, y, num_samples=NUM_SAMPLES, 
+                 num_non_categorical=None):
     """
     Sample from the posteriors of any of the supported models
     
@@ -31,6 +36,10 @@ def sample_model(model_name, X, y, num_samples=NUM_SAMPLES):
         sample_ls_linear(X, y, num_samples)
     elif 'robust_linear' == model_name:
         sample_robust_linear(X, y, num_samples)
+    elif 'ls_pairwise_linear' == model_name:
+        sample_ls_pairwise(X, y, num_non_categorical, num_samples)
+    elif 'ls_quadratic_linear' == model_name:
+        sample_ls_quadratic(X, y, num_non_categorical, num_samples)
     elif 'gp' == model_name:
         sample_gp(X, y, num_samples)
     else:
@@ -66,6 +75,82 @@ def sample_robust_linear(X, y, num_samples=NUM_SAMPLES):
         pm.GLM(X, y, family=StudentT)
         trace = pm.sample(num_samples)
     return format_trace(trace)
+
+
+def sample_ls_pairwise(X, y, num_non_categorical=None, num_samples=NUM_SAMPLES):
+    """
+    Sample from Bayesian Least Squares Linear Regression that has pairwise
+    interaction terms between all non-categorial variables. Uses Normal
+    likelihood, which is equivalent to minimizing the mean squared error
+    in the frequentist version of Least Squares.
+    
+    Assume all non-categorial variables come first in the ordering.
+    
+    Assume all variables are non-categorical if num_non_categorical isn't
+    given.
+    """
+    d = X.shape[1]
+    if num_non_categorical is None:
+        num_non_categorical = d
+    data_df = numpy_to_dataframe(X, y)
+    with pm.Model() as model_glm:
+        x_formula = _join_nonempty(
+            (get_pairwise_formula(num_non_categorical),
+             get_linear_formula(num_non_categorical + 1, d))
+        )
+        pm.GLM.from_formula('y ~ ' + x_formula, data_df)
+        trace = pm.sample(num_samples)
+    return format_trace(trace)
+    
+
+def sample_ls_quadratic(X, y, num_non_categorical=None, num_samples=NUM_SAMPLES):
+    """
+    Sample from Bayesian Least Squares Linear Regression that has all first and
+    second order terms. Uses Normal likelihood, which is equivalent to
+    minimizing the mean squared error in the frequentist version of Least
+    Squares.
+    
+    Assume all non-categorial variables come first in the ordering.
+    
+    Assume all variables are non-categorical if num_non_categorical isn't
+    given.
+    """
+    d = X.shape[1]
+    if num_non_categorical is None:
+        num_non_categorical = d
+    data_df = numpy_to_dataframe(X, y)
+    with pm.Model() as model_glm:
+        x_formula = _join_nonempty(
+            (get_quadratic_formula(num_non_categorical),
+             get_linear_formula(num_non_categorical + 1, d))
+        )
+        pm.GLM.from_formula('y ~ ' + x_formula, data_df)
+        trace = pm.sample(num_samples)
+    return format_trace(trace)
+
+
+def get_pairwise_formula(num_non_categorical):
+    var_names = get_var_names(num_non_categorical)
+    singles_str = ' + '.join(var_names)
+    pairs_str = ' + '.join(':'.join(pair)
+                           for pair in combinations(var_names, 2))
+    return _join_nonempty((singles_str, pairs_str))
+
+
+def get_quadratic_formula(num_non_categorical):
+    pairwise_str = get_pairwise_formula(num_non_categorical)
+    var_names = get_var_names(num_non_categorical)
+    squares_str = ' + '.join('np.power(x{}, 2)'.format(i)
+                             for i in range(1, num_non_categorical + 1))
+    return _join_nonempty((pairwise_str, squares_str))
+
+
+def get_linear_formula(start_i, end_i):
+    return ' + '.join('x' + str(i) for i in range(start_i, end_i + 1))
+
+
+def _join_nonempty(l):
+    return ' + '.join(s for s in l if s != '')
 
 
 def sample_gp(X, y, num_samples=NUM_SAMPLES):
