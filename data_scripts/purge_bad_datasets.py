aedb9f76d5efe82c1a3e4a16e6cf024f9609b3c4
Brady Neal <bradyneal11@gmail.com>
2017-08-01 09:49:18 -0400

    Make minor changes to purge_bad_datasets script

--- data_scripts/purge_bad_datasets.py
+++ data_scripts/purge_bad_datasets.py
@@ -1,6 +1,6 @@
 """
 Read all downloaded datasets, attempt to redownload any that fail to load,
-and purge any that fail to redownload..
+and purge any that fail to redownload.
 """
 
 import sys
@@ -15,10 +15,9 @@ from data.repo import download_dataset
 from data.config import Preprocess
 
 
-def purge_bad_datasets():
+def purge_bad_datasets(start_i=0):
     dataset_ids = get_downloaded_dataset_ids()
     num_datasets = len(dataset_ids)
-    start_i = 0
     for i in range(start_i, num_datasets):
         print('{} of {}'.format(i + 1, num_datasets), end='\t')
         dataset_id = dataset_ids [i]
