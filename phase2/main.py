3dcf53f381c025ac48ca99bd1844e9021ad9f602
Ryan Turner <turnerry@eos16.iro.umontreal.ca>
2017-09-02 15:51:34 -0400

    adding first version of code to do the training.

---
+++ phase2/main.py
@@ -0,0 +1,71 @@
+# Ryan Turner (turnerry@iro.umontreal.ca)
+import cPickle
+import os
+import sys
+import numpy as np
+from model_wrappers import PARAM_EXTRACTORS, STD_BENCH_MODELS
+
+
+def main():
+    '''This program can be run in parallel across different MC_chain files
+    indep. This is a top level routine so I am not worried about needing a
+    verbosity setting.'''
+    # The config file might become another cmd line arg and might go at top
+    # level since it will be shared across phases
+    input_path = '.'  # TODO Get path for files from config file
+    output_path = '.'  # TODO Get path for files from config file
+    pkl_ext = '.pkl'  # TODO PKL_EXT goes in the config file too
+
+    assert(len(sys.argv) == 2)  # Print usage error instead to be user friendly
+    mc_chain_file = sys.argv[1]
+    mc_chain_file = os.path.join(input_path, mc_chain_file)
+    print 'loading %s' % mc_chain_file
+
+    # Use np to directly load in csv, if this becomes a problem then use pandas
+    # and then .values to get an np array out. Set to raise an error if
+    # anything weird in file.
+    MC_chain = np.genfromtxt(mc_chain_file, dtype=float, delimiter=',',
+                             skip_header=0, loose=False, invalid_raise=True)
+    print 'size %d x %d' % (MC_chain.shape[0], MC_chain.shape[1])
+    # We could also print a scipy describe function or something to be fancy
+    # TODO setup based on a fraction arg or something, maybe from config
+    N_train = int(np.ceil(0.8 * MC_chain.shape[0]))
+
+    best_loglik = -np.inf
+    best_case = None
+    for model_name, bench_model in STD_BENCH_MODELS.iteritems():
+        print 'running %s' % model_name
+
+        # leave at default params for now, can use fancy skopt stuff later.
+        # All models are setup in sklearn pattern to make later use with skopt
+        # easier, and can use sklearn estimators with no wrappers.
+        model = bench_model()
+        model.fit(MC_chain[:N_train, :])
+        # Get score for each sample, can then use benchmark tools for table
+        # with error bars and all that at a later point.
+        loglik_vec = model.score_samples(MC_chain[N_train:, :])
+
+        test_loglik = np.mean(loglik_vec)
+        print '%s: %f' % (model_name, test_loglik)
+        if test_loglik > best_loglik:
+            best_loglik = test_loglik
+            best_case = (model_name, model)
+    assert(best_case is not None)
+
+    model_name, model = best_case
+    print 'using %s' % model_name
+
+    # There exist methods to pickle sklearn learns object, but these systems
+    # seem brittle.  We also need to re-implement these objects anyway for
+    # reuse with pymc3, so we might as well just save the parameters clean.
+    params_obj = PARAM_EXTRACTORS[model_name](model)
+
+    # Now dump to finish the job
+    dump_file = os.path.join(output_path, mc_chain_file) + pkl_ext
+    print 'saving %s' % dump_file
+    with open(dump_file, 'wb') as f:
+        cPickle.dump((model_name, params_obj), f, cPickle.HIGHEST_PROTOCOL)
+    print 'done'
+
+if __name__ == '__main__':
+    main()
---
+++ phase2/model_wrappers.py
@@ -0,0 +1,217 @@
+# Ryan Turner (turnerry@iro.umontreal.ca)
+import os
+import numpy as np
+from sklearn.mixture import GaussianMixture
+
+# IGN stuff
+import bench_models.ign.ign as ign
+import bench_models.ign.t_util as t_util
+
+# RNADE stuff
+from bench_models.nade.Data import Dataset, BigDataset
+import bench_models.nade.NADE as NADE
+import bench_models.nade.Instrumentation as Instrumentation
+import bench_models.nade.Backends as Backends
+import bench_models.nade.Optimization as Optimization
+import bench_models.nade.TrainingController as TrainingController
+from bench_models.nade.Utils.DropoutMask import create_dropout_masks
+from bench_models.nade.Utils.theano_helpers import floatX
+
+# TODO put in wrappers for:
+# simple gauss (diag and full), MoG, RNADE, IGN, RNVP (py2 vs 3 issues?)
+# later: other mixtures (MoT?), RNADE variants like laplace,
+# other implentations of RNVP/RNADE, maybe fancy stuff like DPM
+# factor analysis/PCA also possible as ref points
+
+
+def mog_get_params(mog_obj):
+    '''We could do some sort of inherited objected on
+    sklearn.mixture.GaussianMixture but this seems easier.'''
+    D = {'weights': mog_obj.weights_, 'means': mog_obj.means_,
+         'covariances': mog_obj.covariances_, 'type': mog_obj.covariance_type}
+    return D
+
+
+class IGN:
+    def __init__(self, n_layers=1, WL_init=1e-2, reg_dict={}, valid_frac=0.2,
+                 gauss_basepdf=True, n_epochs=100, batch_size=32):
+        self.n_layers = n_layers
+        self.WL_init = WL_init
+        self.reg_dict = reg_dict
+        self.n_epochs = n_epochs
+        self.batch_size = batch_size
+        self.valid_frac = valid_frac
+
+        # Only parts that need to be saved in param extraction
+        self.gauss_basepdf = gauss_basepdf
+        self.fit_layers = None
+
+    def fit(self, x_train):
+        N, D = x_train.shape
+        n_train = int(np.ceil((1.0 - self.valid_frac) * N))
+
+        # Could also have setting in there for trying LU vs full training
+        layers = ign.init_ign_LU(self.n_layers, D, WL_val=self.WL_init)
+        # Chunk some in validation
+        base_logpdf = t_util.norm_logpdf_T if self.gauss_basepdf \
+            else t_util.t_logpdf_T
+        R = ign.train_ign(x_train[:n_train, :], x_train[n_train:, :],
+                          layers, self.reg_dict, base_logpdf=base_logpdf,
+                          n_epochs=self.n_epochs, batch_size=self.batch_size)
+        _, _, _, fit_layers = R
+        # Need to convert to non-LU rep here
+        self.fit_layers = ign.LU_to_W_np(layers)
+
+    def score_samples(self, x_test):
+        assert(self.fit_layers is not None)
+        logpdf = ign.ign_log_pdf(x_test, self.fit_layers, self.base_logpdf)
+        return logpdf
+
+    def get_params(self):
+        assert(self.fit_layers is not None)
+        # Make a copy, maybe we should even make a deep copy
+        D = dict(self.fit_layers)
+        D['gauss_basepdf'] = self.gauss_basepdf
+        return D
+
+
+class RNADE:
+    def __init__(self, n_hidden=100, h_layers=2, n_components=10,
+                 pretraining_epochs=5, validation_loops=20, valid_frac=0.2,
+                 lr=0.02, epoch_size=100, batch_size=100, momentum=0.9,
+                 dataset_name='RNADE_test_run', scratch_dir='.'):
+        # TODO eliminate need for these things
+        self.dataset_name = dataset_name
+        self.scratch_dir = scratch_dir
+
+        self.n_hidden = n_hidden
+        self.h_layers = h_layers
+        self.n_components = n_components
+        self.nonlinearity = 'RLU'  # Might just want to hard code this one
+
+        self.pretraining_epochs = pretraining_epochs
+        self.validation_loops = validation_loops
+
+        self.lr = lr
+        self.epoch_size = epoch_size
+        self.batch_size = batch_size
+        self.momentum = momentum
+        self.valid_frac = valid_frac
+
+        self.nade_class = NADE.OrderlessMoGNADE
+        self.nade_obj = None
+
+    def fit(self, x_train):
+        # TODO create short aliases on import for some of the long names here
+        # to avoid these super long lines
+        # TODO change " to ' to keep style consistent
+
+        N, n_visible = x_train.shape
+        n_train = int(np.ceil((1.0 - self.valid_frac) * N))
+
+        training_dataset = Dataset(x_train[:n_train, :])
+        validation_dataset = Dataset(x_train[n_train:, :])
+
+        # Rest of stuff in here copied/adapted from orderlessNADE.py, seems
+        # more complicated than it needs to be.  Can it be made simpler??
+
+        # TODO Maybe we can do this in memory without writing files out
+        masks_filename = self.dataset_name + "." + floatX + ".masks"
+        masks_route = os.path.join(self.scratch_dir, masks_filename)
+        create_dropout_masks(self.scratch_dir, masks_filename, n_visible, ks=1000)
+        masks_dataset = BigDataset(masks_route + ".hdf5", "masks/.*", "masks")
+
+        nade = self.nade_class(n_visible, self.n_hidden, 1, self.n_components, nonlinearity=self.nonlinearity)
+        loss_function = 'sym_masked_neg_loglikelihood_gradient'
+        validation_loss = lambda ins: -ins.model.estimate_average_loglikelihood_for_dataset_using_masks(validation_dataset, masks_dataset, loops=self.validation_loops)
+        validation_loss_measurement = Instrumentation.Function('validation_loss', validation_loss)
+
+        # TODO make optional
+        console = Backends.Console()
+        textfile_log = Backends.TextFile(os.path.join(self.scratch_dir, "NADE_training.log"))
+        hdf5_backend = Backends.HDF5(self.scratch_dir, "NADE")
+
+        # Pretrain layerwise
+        for l in xrange(1, self.h_layers + 1):
+            if l == 1:
+                nade.initialize_parameters_from_dataset(training_dataset)
+            else:
+                nade = self.nade_class.create_from_smaller_NADE(nade, add_n_hiddens=1)
+
+            # Configure training
+            trainer = Optimization.MomentumSGD(nade, nade.__getattribute__(loss_function))
+            trainer.set_datasets([training_dataset, masks_dataset])
+            trainer.set_learning_rate(self.lr)
+            trainer.set_datapoints_as_columns(True)
+            trainer.add_controller(TrainingController.AdaptiveLearningRate(self.lr, 0, epochs=self.pretraining_epochs))
+            trainer.add_controller(TrainingController.MaxIterations(self.pretraining_epochs))
+            # TODO use np.inf in these spots
+            trainer.add_controller(TrainingController.ConfigurationSchedule("momentum", [(2, 0), (float('inf'), self.momentum)]))
+            trainer.set_updates_per_epoch(self.epoch_size)
+            trainer.set_minibatch_size(self.batch_size)
+            trainer.add_controller(TrainingController.NaNBreaker())
+
+            # Instrument the training
+            trainer.add_instrumentation(Instrumentation.Instrumentation([console, textfile_log, hdf5_backend],
+                                                                        Instrumentation.Function("training_loss", lambda ins: ins.get_training_loss())))
+            trainer.add_instrumentation(Instrumentation.Instrumentation([console, textfile_log, hdf5_backend], Instrumentation.Configuration()))
+            trainer.add_instrumentation(Instrumentation.Instrumentation([console, textfile_log, hdf5_backend], Instrumentation.Timestamp()))
+
+            # Train
+            trainer.set_context("pretraining_%d" % l)
+            trainer.train()
+
+        # Configure training
+        ordering = range(n_visible)  # TODO should this be np array??
+        np.random.shuffle(ordering)
+        trainer = Optimization.MomentumSGD(nade, nade.__getattribute__(loss_function))
+        trainer.set_datasets([training_dataset, masks_dataset])
+        trainer.set_learning_rate(self.lr)
+        trainer.set_datapoints_as_columns(True)
+        trainer.add_controller(TrainingController.AdaptiveLearningRate(self.lr, 0, epochs=self.epochs))
+        trainer.add_controller(TrainingController.MaxIterations(self.epochs))
+        trainer.add_controller(TrainingController.ConfigurationSchedule("momentum", [(2, 0), (float('inf'), self.momentum)]))
+        trainer.set_updates_per_epoch(self.epoch_size)
+        trainer.set_minibatch_size(self.batch_size)
+
+        trainer.add_controller(TrainingController.NaNBreaker())
+
+        # Instrument the training
+        trainer.add_instrumentation(Instrumentation.Instrumentation([console, textfile_log, hdf5_backend],
+                                                                    Instrumentation.Function("training_loss", lambda ins: ins.get_training_loss())))
+
+        trainer.add_instrumentation(Instrumentation.Instrumentation([console], validation_loss_measurement))
+        trainer.add_instrumentation(Instrumentation.Instrumentation([hdf5_backend], validation_loss_measurement, at_lowest=[Instrumentation.Parameters()]))
+        trainer.add_instrumentation(Instrumentation.Instrumentation([console, textfile_log, hdf5_backend], Instrumentation.Configuration()))
+        trainer.add_instrumentation(Instrumentation.Instrumentation([console, textfile_log, hdf5_backend], Instrumentation.Timestamp()))
+
+        # Train
+        trainer.train()
+
+        if trainer.was_successful():
+            self.nade_obj = nade
+        else:
+            # TODO convert to real warning
+            print 'Training error in RNADE!'
+
+    def score_samples(self, x_test):
+        assert(self.nade_obj is not None)
+        logpdf = self.nade_obj.logdensity(x_test)
+        assert(logpdf.shape == (x_test.shape[0],))
+        return logpdf
+
+    def get_params(self):
+        assert(self.nade_obj is not None)
+        self.nade_obj.get_parameters()
+
+# TODO consider a default args dict as well
+
+# Dict with sklearn like interfaces for each of the models we will use to train
+# samples.
+STD_BENCH_MODELS = {'MoG': GaussianMixture, 'IGN': IGN, 'RNADE': RNADE}
+
+# Dict with functions to extract out the parameters from each of these models
+# In future, might just want to have each model contain get_params() function
+# and use inheritence for MoG.
+PARAM_EXTRACTORS = {'MoG': mog_get_params, 'IGN': IGN.get_params,
+                    'RNADE': RNADE.get_params}
