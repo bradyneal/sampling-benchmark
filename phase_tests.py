4707d81edc851c92226181f1a806add6ee8242e2
Ryan Turner <turnerry@eos16.iro.umontreal.ca>
2017-09-24 18:50:29 -0400

    added mvn check to phase tests.

--- phase_tests.py
+++ phase_tests.py
@@ -2,6 +2,7 @@
 import cPickle as pkl
 import os
 import numpy as np
+import scipy.stats as ss
 import theano
 import theano.tensor as T
 import phase2_train_benchmarks.model_wrappers as p2
@@ -37,12 +38,47 @@ def test_model(input_path, params_file, N):
     v2 = np.array([logpdf_f(X[ii, :]) for ii in xrange(N)])
     print 'err2 %f' % np.log10(np.max(np.abs(v0 - v2)))
 
+
+def test_mvn(runs=100):
+    err = [0.0, 0.0, 0.0]
+    for rr in xrange(runs):
+        N = np.random.randint(1, 5)
+        D = np.random.randint(1, 5)
+
+        X = np.random.randn(N, D)
+        mu = np.random.randn(D)
+        S = np.random.randn(D, D)
+        S = np.dot(S, S.T)
+
+        logpdf0 = ss.multivariate_normal.logpdf(X, mu, S)
+
+        inv_chol_U_cov = np.linalg.inv(np.linalg.cholesky(S).T)
+
+        logpdf1 = p2.mvn_logpdf_from_chol(X, mu, inv_chol_U_cov)
+        err[0] = max(err[0], np.max(np.abs(logpdf0 - logpdf1)))
+        assert(np.allclose(logpdf0, logpdf1))
+
+        X_tt = T.matrix()
+        logpdf_tt = p3.mvn_logpdf_from_chol(X_tt, mu, inv_chol_U_cov)
+        logpdf_f = theano.function([X_tt], logpdf_tt)
+        logpdf2 = logpdf_f(X)
+        err[1] = max(err[1], np.max(np.abs(logpdf0 - logpdf2)))
+        assert(np.allclose(logpdf0, logpdf2))
+
+        err[2] = max(err[2], np.max(np.abs(logpdf1 - logpdf2)))
+    print 'mvn err P2 %f' % np.log10(err[0])
+    print 'mvn err P3 %f' % np.log10(err[1])
+    print 'mvn err P2-P3 %f' % np.log10(err[2])
+
 np.random.seed(8525)
 
 # TODO go in config
 input_path = './local/phase2'
 # TODO take as arg
 N = 10
+
+# Also test our mvn implementations while we are at it
+test_mvn()
 
 params_file_list = sorted(os.listdir(input_path))
 for params_file in params_file_list:
