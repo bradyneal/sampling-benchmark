35bd35ccdec3c120a14b6b343124bd310bd95cd1
Ryan Turner <turnerry@eos16.iro.umontreal.ca>
2017-09-27 09:47:35 -0400

    also disregard beginning of chain.

--- phase2_train_benchmarks/validate_input_data.py
+++ phase2_train_benchmarks/validate_input_data.py
@@ -5,6 +5,20 @@ import scipy.stats as ss
 import ConfigParser
 import fileio as io
 
+
+def moments_report(X):
+    V = np.var(X, axis=0)
+    var_ratio = np.log10(np.max(V) / np.min(V))
+
+    C = np.cov(X, rowvar=0)
+    cond_number = np.log10(np.linalg.cond(C))
+
+    max_skew = np.max(np.abs(ss.skew(X, axis=0)))
+    max_kurt = np.max(ss.kurtosis(X, axis=0))
+
+    print 'log10 var ratio %f, cond numer %f' % (var_ratio, cond_number)
+    print 'max skew %f, max kurt %f' % (max_skew, max_kurt)
+
 EPSILON = 1e-12
 
 config_file = '../config.ini'
@@ -32,19 +46,14 @@ for chain in chain_files:
     acc_valid = np.all(np.any(acc, 1) == np.all(acc, 1))
     acc_rate = np.mean(acc[:, 0])
 
-    V = np.var(X, axis=0)
-    var_ratio = np.log10(np.max(V) / np.min(V))
-
-    C = np.cov(X, rowvar=0)
-    cond_number = np.log10(np.linalg.cond(C))
-
-    max_skew = np.max(np.abs(ss.skew(X, axis=0)))
-    max_kurt = np.max(ss.kurtosis(X, axis=0))
-
     print chain
     print 'N = %d, D = %d' % (N, D)
     print 'finite %d, accept %d' % (finite, acc_valid)
     print 'acc rate %f' % acc_rate
-    print 'log10 var ratio %f, cond numer %f' % (var_ratio, cond_number)
-    print 'max skew %f, max kurt %f' % (max_skew, max_kurt)
+
+    print 'full data'
+    moments_report(X)
+
+    print '2nd half data'
+    moments_report(X[int(N / 2):, :])
 print 'done'
