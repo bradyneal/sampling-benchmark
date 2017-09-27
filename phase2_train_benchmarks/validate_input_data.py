b6a29e2919ecd7422116f6738b4e81bd8b12ca02
Ryan Turner <turnerry@eos16.iro.umontreal.ca>
2017-09-27 16:14:43 -0400

    some cleanup on phase 2 input validation.

--- phase2_train_benchmarks/validate_input_data.py
+++ phase2_train_benchmarks/validate_input_data.py
@@ -9,6 +9,18 @@ EPSILON = 1e-12
 
 
 def moments_report(X):
+    N, D = X.shape
+
+    finite = np.all(np.isfinite(X))
+
+    acc = np.abs(np.diff(X, axis=0)) > EPSILON
+    acc_valid = np.all(np.any(acc, 1) == np.all(acc, 1))
+    acc_rate = np.mean(acc[:, 0])
+
+    print 'N = %d, D = %d' % (N, D)
+    print 'finite %d, accept %d' % (finite, acc_valid)
+    print 'acc rate %f' % acc_rate
+
     V = np.std(X, axis=0)
     std_ratio = np.log10(np.max(V) / np.min(V))
 
@@ -20,10 +32,17 @@ def moments_report(X):
     max_skew = np.max(np.abs(ss.skew(X, axis=0)))
     max_kurt = np.max(ss.kurtosis(X, axis=0))
 
-    print 'N = %d' % X.shape[0]
-    print 'log10 var ratio %f, cond numer %f' % (std_ratio, cond_number)
+    print 'log10 std ratio %f, cond number %f' % (std_ratio, cond_number)
     print 'min corr %f, max corr %f' % (np.min(corr), np.max(corr))
     print 'max skew %f, max kurt %f' % (max_skew, max_kurt)
+
+
+def moments_report_w_burn(X, burn_frac=0.05):
+    print 'full data'
+    moments_report(X)
+
+    print 'post burn-in'
+    moments_report(X[int(burn_frac * X.shape[0]):, :])
 
 
 def main():
@@ -42,28 +61,11 @@ def main():
 
     for chain in chain_files:
         print '-' * 20
+        print chain
         X = io.load_np(input_path, chain, '')
         assert(X.ndim == 2)
-        N, D = X.shape
-
-        finite = np.all(np.isfinite(X))
-
-        acc = np.abs(np.diff(X, axis=0)) > EPSILON
-        acc_valid = np.all(np.any(acc, 1) == np.all(acc, 1))
-        acc_rate = np.mean(acc[:, 0])
-
-        print chain
-        print 'N = %d, D = %d' % (N, D)
-        print 'finite %d, accept %d' % (finite, acc_valid)
-        print 'acc rate %f' % acc_rate
-
-        print 'full data'
-        moments_report(X)
-
-        print 'post burn-in'
-        moments_report(X[int(0.05 * N):, :])
+        moments_report_w_burn(X)
     print 'done'
 
 if __name__ == '__main__':
     main()
-
