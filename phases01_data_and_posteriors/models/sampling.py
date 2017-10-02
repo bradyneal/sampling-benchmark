d3522d061262a762d58716786e5f04049252e160
Brady Neal <bradyneal11@gmail.com>
2017-10-02 17:06:21 -0400

    Add support for multiple chains (currently broken)

--- phases01_data_and_posteriors/models/sampling.py
+++ phases01_data_and_posteriors/models/sampling.py
@@ -3,8 +3,10 @@ This module provides the sampling, given a model and step.
 """
 
 import pymc3 as pm
+from pymc3.backends.base import merge_traces
 import theano
 from timeit import default_timer as timer
+from functools import partial
 
 from .utils import format_trace
 from . import MAX_NUM_SAMPLES, NUM_INIT_STEPS, SOFT_MAX_TIME_IN_SECONDS, \
@@ -13,9 +15,32 @@ from . import MAX_NUM_SAMPLES, NUM_INIT_STEPS, SOFT_MAX_TIME_IN_SECONDS, \
 
 
 def sample_model(model, step=None, num_samples=MAX_NUM_SAMPLES, advi=False,
-                 num_scale1_iters=NUM_SCALE1_ITERS,
+                 n_chains=NUM_CHAINS, num_scale1_iters=NUM_SCALE1_ITERS,
                  num_scale0_iters=NUM_SCALE0_ITERS):
-    """Sample from constructed Bayesian model"""
+    """Sample parallel chains from constructed Bayesian model"""
+    sample_chain_with_args = partial(
+        sample_chain, step=step, num_samples=num_samples, advi=advi,
+        num_scale1_iters=num_scale1_iters, num_scale0_iters=num_scale0_iters)
+  
+    if advi:
+        return sample_chain_with_args(model)
+    else:
+        traces = []
+        for i in range(n_chains):
+            traces.append(sample_chain_with_args(model, i))
+    
+        print('traces:')
+        print(traces)
+        merged = merge_traces(traces)
+        print('merged:')
+        print(merged)
+        return merged
+
+
+def sample_chain(model, chain_i=0, step=None, num_samples=MAX_NUM_SAMPLES,
+                 advi=False, num_scale1_iters=NUM_SCALE1_ITERS,
+                 num_scale0_iters=NUM_SCALE0_ITERS):
+    """Sample single chain from constructed Bayesian model"""
     start = timer()
     with model:
         if not advi:
@@ -24,10 +49,11 @@ def sample_model(model, step=None, num_samples=MAX_NUM_SAMPLES, advi=False,
                 start_, step = pm.init_nuts(init='advi', njobs=1, n_init=NUM_INIT_STEPS,
                                             random_seed=-1, progressbar=False)
             
-            for i, trace in enumerate(pm.iter_sample(num_samples, step)):
+            for i, trace in enumerate(pm.iter_sample(
+                num_samples, step, chain=chain_i)):
                 if i == 0:
-                    min_num_samples = get_min_num_samples(len(trace[0]),
-                                                          MIN_SAMPLES_CONSTANT)
+                    min_num_samples = get_min_samples_per_chain(
+                        len(trace[0]), MIN_SAMPLES_CONSTANT, NUM_CHAINS)
                 elapsed = timer() - start
                 if elapsed > SOFT_MAX_TIME_IN_SECONDS:
                     print('exceeded soft time limit...')
@@ -48,11 +74,12 @@ def sample_model(model, step=None, num_samples=MAX_NUM_SAMPLES, advi=False,
             approx = pm.fit(n=num_scale0_iters)
             # one sample to get dimensions of trace
             trace = approx.sample(draws=1)
-            min_num_samples = get_min_num_samples(len(trace.varnames),
-                                                  MIN_SAMPLES_CONSTANT)
+            min_num_samples = get_min_samples_per_chain(
+                len(trace.varnames), MIN_SAMPLES_CONSTANT, 1)
             trace = approx.sample(draws=min_num_samples)
-    return format_trace(trace)
+            
+    return trace
 
 
-def get_min_num_samples(dimension, min_samples_constant):
-    return min_samples_constant * (dimension ** 2)
+def get_min_samples_per_chain(dimension, min_samples_constant, n_chains):
+    return int(min_samples_constant * (dimension ** 2) / n_chains)
