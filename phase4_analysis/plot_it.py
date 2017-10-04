c5178cfe1771a2a75984b3b479413dc37ab66895
Ryan Turner <turnerry@eos16.iro.umontreal.ca>
2017-10-03 22:14:43 -0400

    working on plotting script.

--- phase4_analysis/plot_it.py
+++ phase4_analysis/plot_it.py
@@ -10,11 +10,72 @@ R['real_ess'] = ref / R['ks']
 R['eff'] = R['real_ess'] / R['N']
 '''
 
+# rcParams['axes.color_cycle']
+# from matplotlib import rcParams
+# TODO plot on outside
+
+# TODO do groupby to aggregate over dims
+# TODO adds lines between dims on same example
+
+def plot_ess(df, groupby_col, metric, pooled=False):
+    gdf = df.groupby(groupby_col)
+    metric = metric + '_pooled' if pooled else metric
+
+    plt.figure()
+    for name, sub_df in gdf:
+        n_chains = sub_df['n_chains'].values
+
+        real_ess = 1.0 / sub_df[metric].values  # TODO change ref in num
+        estimated_ess = sub_df['ESS'].values if pooled else \
+            sub_df['ESS'].values / n_chains
+        plt.loglog(estimated_ess, real_ess, '.', label=name, alpha=0.5)
+    xgrid = np.logspace(0.0, 6.0, 100)  # TODO automatic
+    plt.loglog(xgrid, xgrid, 'k--')
+    plt.legend()
+    plt.grid('on')
+    plt.xlabel('ESS')
+    plt.ylabel('real ESS')
+    plt.title(metric)
+
+
+
+def plot_eff(df, groupby_col, metric, pooled=False):
+    gdf = df.groupby(groupby_col)
+    metric = metric + '_pooled' if pooled else metric
+
+    plt.figure()
+    for name, sub_df in gdf:
+        n_chains = sub_df['n_chains'].values
+        n_samples = sub_df['N'].values
+
+        real_ess = 1.0 / sub_df[metric].values  # TODO change ref in num
+        real_eff = real_ess / n_samples
+
+        estimated_ess = sub_df['ESS'].values if pooled else \
+            sub_df['ESS'].values / n_chains
+        estimated_eff = estimated_ess / n_samples
+
+        plt.loglog(estimated_eff, real_eff, '.', label=name, alpha=0.5)
+    xgrid = np.logspace(-6.0, 0.0, 100)  # TODO automatic
+    plt.loglog(xgrid, xgrid, 'k--')
+    plt.legend()
+    plt.grid('on')
+    plt.xlabel('estimated efficiency')
+    plt.ylabel('real efficiency')
+    plt.title(metric)
+
+
+
 
 # TODO config
-fname = '../../sampler-local/full_size/phase4/perf_sync.csv'
+#fname = '../../sampler-local/full_size/phase4/perf_sync.csv'
+
+fname = '../perf_sync.csv'
 df = pd.read_csv(fname, header=0, index_col=None)
 
+
+
+'''
 examples = df['example'].unique()
 samplers = df['sampler'].unique()
 
@@ -78,3 +139,4 @@ plt.legend()
 plt.xlabel('estimated efficiency')
 plt.ylabel('real efficiency')
 plt.title('mean pooled')
+'''
