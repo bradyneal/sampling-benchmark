62a3d09ae76e2b134f4e346ad15689f43e1d3a35
Brady Neal <bradyneal11@gmail.com>
2017-10-10 11:02:56 -0400

    Update phase 2 batch SLURM script with proper environment and job names

--- phase2_train_benchmarks/slurm_job.sh
+++ phase2_train_benchmarks/slurm_job.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=sample_posteriors
+#SBATCH --job-name=phase2
 #SBATCH --mem=128g
 #SBATCH --mem-per-cpu=8192
 
@@ -8,5 +8,5 @@ source ~/.bashrc
 export PYTHONUNBUFFERED=1
 echo Running on $HOSTNAME
 
-source activate samp-phase02
+source activate samp-phase2
 python run_all.py
