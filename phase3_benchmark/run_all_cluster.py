40a603ece4f612f37c4b5c5895d6b94176ba1974
Brady Neal <bradyneal11@gmail.com>
2017-10-10 18:21:39 -0400

    Add code to only launch jobs that aren't queued or currently running

--- phase3_benchmark/run_all_cluster.py
+++ phase3_benchmark/run_all_cluster.py
@@ -3,7 +3,8 @@ import sys
 from time import time
 import fileio as io
 from main import run_experiment
-from clusterlib.scheduler import submit
+import os
+from clusterlib.scheduler import submit, queued_or_running_jobs
 # This will import pymc3 which is not needed if the experiments are run in a
 # separate process in the future. Loading pymc3 will be a bit of a waste just
 # to get the dictionary keys. We could re-work this, but prob not worth effort.
@@ -33,16 +34,22 @@ def main():
 
     # Run n_chains in the outer loop since if process get killed we have less
     # chains but with even distribution over models and samplers.
+    scheduled_jobs = set(queued_or_running_jobs())
     for model_name in model_list:
         for _ in xrange(config['n_chains']):
             # TODO could put ADVI init here to keep it fixed across samplers
             for sampler in sampler_list:
                 t = time()
+                job_name = "phase3-%s-%s" % (model_name, sampler)
                 cmd_line_args = (config_file, model_name, sampler)
-                script = submit(
-                    "sbatch -c 1 main.py %s %s %s" % cmd_line_args,
-                    job_name="phase3-%s-%s" % (model_name, sampler),
-                    time="15:00", memory=32000, backend="slurm")
+                if job_name not in scheduled_jobs:
+                    script = submit(
+                        "sbatch -c 1 main.py %s %s %s" % cmd_line_args,
+                        job_name=job_name, time="15:00", memory=32000,
+                        backend="slurm")
+                    
+                    print 'Executing:', script
+                    os.system(script)
                 print 'wall time %fs' % (time() - t)
     print 'done'
 
