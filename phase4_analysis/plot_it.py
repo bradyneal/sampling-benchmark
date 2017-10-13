2e10cb673c1cbc6efb78bbf0eb809a727b07441a
Ryan Turner <turnerry@eos16.iro.umontreal.ca>
2017-10-13 14:29:10 -0400

    possibly version for paper.

--- phase4_analysis/plot_it.py
+++ phase4_analysis/plot_it.py
@@ -16,8 +16,7 @@ import matplotlib.pyplot as plt
 
 DIAGS = ('Geweke', 'ESS', 'Gelman_Rubin')  # TODO import from elsewhere
 
-# TODO re-run with no clip in perf
-#   also adjust MIN_ESS to per chain
+# TODO run with both perf agg that has had rectified and unrectified sq loss
 
 
 def ESS_EB_fac(n_estimates, conf=0.95):
@@ -53,13 +52,15 @@ def aggregate_df(df, diag_agg=np.mean):
     return R
 
 
-def augment_df(df):
+def augment_df(df, success_thold=12):
     # Note: this is happening inplace!
     n_ref = df.groupby('example')['N'].median()
     df['n_ref'] = df['example'].map(n_ref)
+    df['short_name'] = df['sampler'].str.split('-').str[0]
 
     df['ESS_pooled'] = df['ESS']
     df['ESS'] = df['ESS_pooled'] / df['n_chains']
+    df['success'] = df['ESS'] > success_thold
 
     df['NESS_pooled'] = df['ESS_pooled'] / df['n_ref']
     df['NESS'] = df['ESS'] / df['n_ref']
@@ -77,6 +78,7 @@ def augment_df(df):
         df['real_eff_' + metric] = df['real_ess_' + metric] / df['N']
         df['real_essd_' + metric] = ESS_deviation(df[metric].values,
             df['ESS'].values, df['n_chains'].values, metric_ref)
+        df['success_' + metric] = df['real_ess_' + metric] > success_thold
 
         metric = metric + '_pooled'
         df['real_ess_' + metric] = metric_ref / df[metric]
@@ -90,18 +92,22 @@ def augment_df(df):
 
 def plot_by(df, x, y, by, fac_lines=(1.0,)):
     # TODO make logscale arg
-    plt.figure()
+    fig = plt.figure(figsize=(3.5, 3.5), dpi=80, facecolor='w', edgecolor='k')
+    ax = fig.add_axes([0.15, 0.15, 0.8, 0.7])
+
     gdf = df.groupby(by)
     for name, sdf in gdf:
-        plt.loglog(sdf[x].values, sdf[y].values, '.', label=name, alpha=0.5)
+        ax.loglog(sdf[x].values, sdf[y].values, '.', label=name, alpha=0.5)
     xgrid = np.logspace(np.log10(df[x].min()), np.log10(df[x].max()), 100)
     for f in fac_lines:
-        plt.loglog(xgrid, f * xgrid, 'k--')
+        ax.loglog(xgrid, f * xgrid, 'k--')
     plt.legend(loc=3, ncol=4, fontsize=6,
                bbox_to_anchor=(0.0, 1.02, 1.0, 0.102))
     plt.grid()
-    plt.xlabel(x)
-    plt.ylabel(y)
+    ax.tick_params(labelsize=6)
+    plt.xlabel(x, fontsize=10)
+    plt.ylabel(y, fontsize=10)
+    return ax
 
 
 def all_pos_finite(X):
@@ -133,44 +139,44 @@ def try_models(df_train, df_test, metric, feature_list, target, methods):
     assert(all_finite(X_test))
 
     assert(target.endswith(metric))
-    y_train = df_train[target].values
-    y_test = df_test[target].values
+    y_train = np.clip(df_train[target].values, -5, 5)
+    y_test = np.clip(df_test[target].values, -5, 5)
     assert(all_finite(y_train))
     assert(all_finite(y_test))
 
     pred_tbl = btr.get_gauss_pred(X_train, y_train, X_test, methods)
     loss_tbl = btr.loss_table(pred_tbl, y_test, loss_dict)
-    return loss_tbl
+    return loss_tbl, scaler
 
 
 def run_experiment(df, metric, split_dict, all_features, target,
                    ref_method='GPR'):
     # TODO also do MLP
-    k = 1.0 * RBF(length_scale=np.ones(len(all_features))) + \
-        WhiteKernel(noise_level=0.1**2, noise_level_bounds=(1e-3, np.inf))
-    methods = {'iid': btr.JustNoise(), 'linear': BayesianRidge(),
-               'GPR': GaussianProcessRegressor(kernel=k)}
-    # Really dumb but GPR kernel needs to know D in advance
-    k_sub = 1.0 * RBF(length_scale=np.ones(len(all_features) - 1)) + \
-        WhiteKernel(noise_level=0.1**2, noise_level_bounds=(1e-3, np.inf))
-
     summary = {}
+    ref_model = {}
     for split_name, splits in split_dict.iteritems():
         print split_name
         df_train, df_test, _ = ds.split_df(df, splits=splits)
 
-        loss_tbl = \
+        k = 1.0 * RBF(length_scale=np.ones(len(all_features))) + \
+            WhiteKernel(noise_level=0.1**2, noise_level_bounds=(1e-3, np.inf))
+        methods = {'iid': btr.JustNoise(), 'linear': BayesianRidge(),
+                   'GPR': GaussianProcessRegressor(kernel=k)}
+        loss_tbl, scaler = \
             try_models(df_train, df_test, metric, all_features, target, methods)
+        ref_model[split_name] = (methods[ref_method], scaler)
 
         # Could also try just removing one
         L = [loss_tbl]
         for feature in all_features:
             mname = 'GPR-' + feature
+            k_sub = 1.0 * RBF(length_scale=np.ones(len(all_features) - 1)) + \
+                WhiteKernel(noise_level=0.1**2, noise_level_bounds=(1e-3, np.inf))
             methods_sub = {mname: GaussianProcessRegressor(kernel=k_sub)}
             sub_f = list(all_features)
             sub_f.remove(feature)
-            loss_tbl = try_models(df_train, df_test,
-                                  metric, sub_f, target, methods_sub)
+            loss_tbl, _ = try_models(df_train, df_test,
+                                     metric, sub_f, target, methods_sub)
             L.append(loss_tbl)
 
         # Aggregate
@@ -180,40 +186,81 @@ def run_experiment(df, metric, split_dict, all_features, target,
         # TODO use const names
         full_tbl[('NLL', 'mean')] -= full_tbl.loc[ref_method, ('NLL', 'mean')]
         summary[split_name] = full_tbl
-    return summary
+    return summary, ref_model
+
+
+def ecdf(x):
+    plt.plot(np.sort(x), np.linspace(0, 1, len(x), endpoint=False))
 
 # TODO config
-fname = '../../sampler-local/full_size/phase4_initial_tests/perf_sync.csv'
+#fname = '../../sampler-local/full_size/phase4_initial_tests/perf_sync.csv'
+#fname = '/u/turnerry/Documents/git/sampler-local/graham/phase4/perf_sync.csv'
+#fname = '/data/lisatmp3/turnerry/sampling-benchmark/cedar/phase4/perf_sync.csv'
+fname = './cedar1_P4/perf_sync.csv'
 
 np.random.seed(56456)
 do_plots = True
 
 df = pd.read_csv(fname, header=0, index_col=None)
+df = df[df['n_chains'] > 1]
 
 agg_df = aggregate_df(df)
 df = augment_df(df)
 
+metric_list = sorted(METRICS_REF.keys())
+gdf = df.groupby('sampler')
+cols = ['success'] + ['success_' + m for m in metric_list]
+success_tbl = gdf[cols].mean()
+print success_tbl.to_string()
+print success_tbl.to_latex()
+
+NESS_tbl = gdf[['real_ness_' + m for m in metric_list]].mean()
+
+print pd.concat((NESS_tbl, success_tbl), axis=1).to_latex(float_format='%.3f')
+
 if do_plots:
-    n_chains = df['n_chains'].min()
-    if n_chains != df['n_chains'].max():
+    n_chains = df['n_chains'].max()
+    if n_chains != df['n_chains'].min():
         print 'warning! error bars assume n_chains constant'
     lb, ub = ESS_EB_fac(n_chains)
 
-    plot_by(df, 'ESS', 'real_ess_mean', 'sampler', (lb, 1.0, ub))
+    # Calibration plots
+    df_sub = df[(df['n_chains'] == n_chains) & (df['success'])]
+    for metric in metric_list:
+        plot_by(df_sub, 'ESS', 'real_ess_' + metric, 'short_name', (lb, 1.0, ub))
+        plt.savefig('real_ess_' + metric + '.pdf', dpi=300, format='pdf')
 
-    plot_by(df, 'eff', 'real_eff_mean', 'sampler')
+        ax = plot_by(df_sub, 'eff', 'real_eff_' + metric, 'short_name')
+        ax.set_xscale('linear')
+        plt.savefig('real_eff_' + metric + '.pdf', dpi=300, format='pdf')
 
-    ax = df.boxplot('real_ness_mean', by='sampler', rot=90)
-    ax.set_yscale('log')
-    plt.xlabel('sampler')
-    plt.ylabel('real_ness_mean')
-    plt.show()
+    # Box plots
+    for metric in metric_list:
+        df_sub = df[df['success_' + metric]]
 
-    ax = df.boxplot('real_eff_mean', by=['sampler', 'D'], rot=90)
-    ax.set_yscale('log')
-    plt.xlabel('sampler')
-    plt.ylabel('real_ness_mean')
-    plt.show()
+        fig = plt.figure(figsize=(3.5, 3.5), dpi=80, facecolor='w', edgecolor='k')
+        ax = fig.add_axes([0.15, 0.2, 0.8, 0.75])
+        df_sub.boxplot('real_ness_' + metric, by='short_name', rot=45, ax=ax)
+        plt.title('')
+        plt.suptitle('')
+        ax.set_yscale('log')
+        ax.tick_params(labelsize=6)
+        plt.xlabel('sampler', fontsize=10)
+        plt.ylabel('NESS ' + metric, fontsize=10)
+        plt.show()
+        plt.savefig('ness_' + metric + '.pdf', dpi=300, format='pdf')
+
+        fig = plt.figure(figsize=(3.5, 3.5), dpi=80, facecolor='w', edgecolor='k')
+        ax = fig.add_axes([0.15, 0.2, 0.8, 0.75])
+        df_sub.boxplot('real_eff_' + metric, by='short_name', rot=45, ax=ax)
+        plt.title('')
+        plt.suptitle('')
+        ax.set_yscale('log')
+        ax.tick_params(labelsize=6)
+        plt.xlabel('sampler', fontsize=10)
+        plt.ylabel('EFF ' + metric, fontsize=10)
+        plt.show()
+        plt.savefig('box_eff_' + metric + '.pdf', dpi=300, format='pdf')
 
 split_dict = {'random': {ds.INDEX: (ds.RANDOM, 0.8)},
               'example': {'example': (ds.RANDOM, 0.8)}}
@@ -222,17 +269,13 @@ metric = 'mean'
 target = 'real_essd_mean'
 all_features = ['TG', 'TGR', 'ESS', 'D']
 
-dumb_sampler = (df['sampler'] == 'emcee')
-print dumb_sampler.sum()
-small_ESS = (df['ESS'] <= 25)
-print small_ESS.sum()
-df_anal = df.loc[~(dumb_sampler | small_ESS), :]
+df_anal = df[df['success']]
 
 missing = df_anal[all_features + [target]].isnull().any(axis=1)
 df_anal = df_anal[~missing]
 assert(not df_anal.isnull().any().any())
 
-summary = run_experiment(df_anal, metric, split_dict, all_features, target)
+summary, ref_model = run_experiment(df_anal, metric, split_dict, all_features, target)
 for split, tbl in summary.iteritems():
     print split
 
